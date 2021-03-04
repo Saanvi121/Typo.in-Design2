95e159864aa48bb58682e091381d111ac200a1e8
Saanvi121 <71918687+Saanvi121@users.noreply.github.com>
2021-03-04 20:02:59 +0530

    Add files via upload

---
+++ typo.js
@@ -0,0 +1,7 @@
+function addUser()
+{
+    user_name = document.getElementById("user_name").value
+    localStorage.setItem("user_name", user_name)
+    window.location = "typo_room.html"
+    document.getElementById("username").innerHTML = "Welcome " + user_name + "!"
+}
